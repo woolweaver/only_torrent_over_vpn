da52dd0209e0ba9eb204a157f3aeb57949ca08a2
Michael Woolweaver <m.woolweaver@icloud.com>
2020-07-24 15:16:48 -0500

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
+
 VPNIF="tun0"
 VPNUSER="vpnuser"
 GATEWAYIP=`ifconfig $VPNIF | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | egrep -v '255|(127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})' | tail -n1`
+
 if [[ `ip rule list | grep -c 0x1` == 0 ]]; then
 ip rule add from all fwmark 0x1 lookup $VPNUSER
 fi
+
 ip route replace default via $GATEWAYIP table $VPNUSER 
+
 ip route append default via 127.0.0.1 dev lo table $VPNUSER
+
 ip route flush cache
